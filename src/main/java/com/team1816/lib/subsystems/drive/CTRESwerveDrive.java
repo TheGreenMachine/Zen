0325f79fe6dfaf9fcb7cdad890a22e719d98f634
mp768 <60555816+mp768@users.noreply.github.com>
2024-01-16 19:54:12 -0600

    I don't know what I'm doing.
    I'm just throwing stuff together and hoping that the logic is logical. I
    want this to work. I don't know how it works. It's confusing.
    
    I've never worked with the swerve stuff, so it might just take a little
    getting used to.

--- src/main/java/com/team1816/lib/subsystems/drive/CTRESwerveDrive.java
+++ src/main/java/com/team1816/lib/subsystems/drive/CTRESwerveDrive.java
@@ -2,6 +2,7 @@ package com.team1816.lib.subsystems.drive;
 
 import com.ctre.phoenix6.mechanisms.swerve.SwerveDrivetrain;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveDrivetrainConstants;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import com.team1816.lib.Infrastructure;
@@ -9,12 +10,80 @@ import com.team1816.lib.hardware.PIDSlotConfiguration;
 import com.team1816.lib.subsystems.LedManager;
 import com.team1816.lib.util.logUtil.GreenLogger;
 import com.team1816.lib.util.team254.DriveSignal;
+import com.team1816.season.configuration.Constants;
 import com.team1816.season.states.RobotState;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
 
 @Singleton
 public class CTRESwerveDrive extends Drive {
+    /** Constants */
+
+    /**
+     * Module Characterization
+     */
+    private static final double moduleDeltaX = kDriveWheelbaseLengthMeters / 2.0;
+    private static final double moduleDeltaY = kDriveWheelTrackWidthMeters / 2.0;
+
+    // module indices
+    public static final int kFrontLeft = 0;
+    public static final int kFrontRight = 1;
+    public static final int kBackLeft = 2;
+    public static final int kBackRight = 3;
+
+    // module positions
+    public static final Translation2d kFrontLeftModulePosition = new Translation2d(
+            moduleDeltaX,
+            moduleDeltaY
+    );
+    public static final Translation2d kFrontRightModulePosition = new Translation2d(
+            moduleDeltaX,
+            -moduleDeltaY
+    );
+    public static final Translation2d kBackLeftModulePosition = new Translation2d(
+            -moduleDeltaX,
+            moduleDeltaY
+    );
+    public static final Translation2d kBackRightModulePosition = new Translation2d(
+            -moduleDeltaX,
+            -moduleDeltaY
+    );
+
+    public static final Translation2d[] kModulePositions = {
+            kFrontLeftModulePosition,
+            kFrontRightModulePosition,
+            kBackRightModulePosition,
+            kBackLeftModulePosition,
+    };
+
+    // Kinematics (https://docs.wpilib.org/en/stable/docs/software/kinematics-and-odometry/swerve-drive-kinematics.html)
+    public static final SwerveDriveKinematics swerveKinematics = new SwerveDriveKinematics(
+            kFrontLeftModulePosition,
+            kFrontRightModulePosition,
+            kBackLeftModulePosition,
+            kBackRightModulePosition
+    );
+
+    /**
+     * Components
+     */
+    public SwerveModule[] swerveModules;
+
+    /**
+     * States
+     */
+    public SwerveModuleState[] desiredModuleStates = new SwerveModuleState[4];
+    SwerveModuleState[] actualModuleStates = new SwerveModuleState[4];
+    SwerveModulePosition[] actualModulePositions = new SwerveModulePosition[4];
+    public double[] motorTemperatures = new double[4];
+
     private SwerveDrivetrain train;
+
+    private SwerveRequest request;
 
     @Inject
     public CTRESwerveDrive(LedManager lm, Infrastructure inf, RobotState rs) {
@@ -25,16 +94,40 @@ public class CTRESwerveDrive extends Drive {
         constants.Pigeon2Id = 32;
 
         train = new SwerveDrivetrain(constants);
+        request = new SwerveRequest.Idle();
     }
 
     @Override
     public synchronized void writeToHardware() {
-
+        if (controlState == ControlState.OPEN_LOOP) {
+            train.setControl(request);
+        }
     }
 
     @Override
     public synchronized void readFromHardware() {
         super.readFromHardware();
+        double[] actualStates = new double[8];
+        double[] desiredStates = new double[8];
+        for (int i = 0; i < 4; i++) {
+            // logging actual angle and velocity of swerve motors (azimuth & drive)
+            swerveModules[i].update();
+            actualModuleStates[i] = swerveModules[i].getActualState();
+            actualModulePositions[i] = swerveModules[i].getActualPosition();
+            // logging current temperatures of each module's drive motor
+            motorTemperatures[i] = swerveModules[i].getMotorTemp();
+
+            if (Constants.kLoggingDrivetrain) {
+                // populating double list with actState angles and speeds
+                actualStates[i * 2] = actualModuleStates[i].angle.getRadians();
+                actualStates[i * 2 + 1] = actualModuleStates[i].speedMetersPerSecond;
+
+                // populating double list with desState angles and speeds
+                desiredStates[i * 2] = desiredModuleStates[i].angle.getRadians();
+                desiredStates[i * 2 + 1] = desiredModuleStates[i].speedMetersPerSecond;
+            }
+        }
+        chassisSpeed = swerveKinematics.toChassisSpeeds(actualModuleStates);
 
         updateRobotState();
     }
@@ -74,12 +167,19 @@ public class CTRESwerveDrive extends Drive {
 
     @Override
     public void setTeleopInputs(double forward, double strafe, double rotation) {
+        SwerveRequest.ApplyChassisSpeeds speeds_request = new SwerveRequest.ApplyChassisSpeeds();
+
+        speeds_request.Speeds = chassisSpeed;
 
     }
 
     @Override
-    public void setBraking(boolean srtBrake) {
+    public synchronized void setBraking(boolean braking) {
+        isBraking = braking;
 
+        if (braking) {
+            request = new SwerveRequest.SwerveDriveBrake();
+        }
     }
 
     @Override
