2de4af3f4ff23d8df6a592577ce7d6e14274ef28
mp768 <60555816+mp768@users.noreply.github.com>
2024-01-19 19:56:27 -0600

    I'm going to have to find a way to get the chasis from the swervedrivetrain.

--- src/main/java/com/team1816/lib/subsystems/drive/CTRESwerveDrive.java
+++ src/main/java/com/team1816/lib/subsystems/drive/CTRESwerveDrive.java
@@ -1,27 +1,40 @@
 package com.team1816.lib.subsystems.drive;
 
+import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveDrivetrain;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveDrivetrainConstants;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import com.team1816.lib.Infrastructure;
+import com.team1816.lib.auto.Color;
+import com.team1816.lib.auto.Symmetry;
 import com.team1816.lib.hardware.PIDSlotConfiguration;
 import com.team1816.lib.subsystems.LedManager;
 import com.team1816.lib.util.logUtil.GreenLogger;
 import com.team1816.lib.util.team254.DriveSignal;
+import com.team1816.lib.util.team254.SwerveDriveHelper;
 import com.team1816.lib.util.team254.SwerveDriveSignal;
+import com.team1816.season.Robot;
 import com.team1816.season.configuration.Constants;
 import com.team1816.season.states.RobotState;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
-import edu.wpi.first.math.kinematics.SwerveModulePosition;
-import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.math.kinematics.*;
+import edu.wpi.first.util.datalog.DoubleLogEntry;
+
+import java.util.List;
 
 @Singleton
 public class CTRESwerveDrive extends Drive {
+    private StatusSignal<Double>[] motorTemperatures;
+
+    /**
+     * Logging
+     */
+    private DoubleLogEntry temperatureLogger;
+
     private SwerveDrivetrain train;
 
     private SwerveRequest request;
@@ -49,31 +62,73 @@ public class CTRESwerveDrive extends Drive {
     public synchronized void readFromHardware() {
         super.readFromHardware();
 
+        for (int i = 0; i < 4; i++) {
+            motorTemperatures[i] = train.getModule(i).getDriveMotor().getDeviceTemp();
+        }
+
         updateRobotState();
     }
 
     @Override
     public void updateRobotState() {
+        robotState.fieldToVehicle = train.getState().Pose;
+        robotState.driverRelativeFieldToVehicle = new Pose2d( // for inputs ONLY
+                robotState.fieldToVehicle.getTranslation(),
+                (robotState.allianceColor == Color.BLUE && Constants.fieldSymmetry == Symmetry.AXIS) ? robotState.fieldToVehicle.getRotation() : robotState.fieldToVehicle.getRotation().rotateBy(Rotation2d.fromDegrees(180))
+        );
 
+        var cs = new ChassisSpeeds(
+                chassisSpeed.vxMetersPerSecond,
+                chassisSpeed.vyMetersPerSecond,
+                chassisSpeed.omegaRadiansPerSecond
+        );
+        robotState.calculatedVehicleAccel =
+                new ChassisSpeeds(
+                        (cs.vxMetersPerSecond - robotState.deltaVehicle.vxMetersPerSecond) /
+                                Robot.looperDt,
+                        (cs.vyMetersPerSecond - robotState.deltaVehicle.vyMetersPerSecond) /
+                                Robot.looperDt,
+                        -9.80
+                );
+        robotState.deltaVehicle = cs;
+
+        temperatureLogger.append(motorTemperatures[0].getValueAsDouble());
+        robotState.drivetrainTemp = motorTemperatures[0].getValueAsDouble();
+
+        robotState.vehicleToFloorProximityCentimeters = infrastructure.getMaximumProximity();
+
+//        swerveOdometry.update(Rotation2d.fromDegrees(train.getPigeon2().getAngle()));
+
+        if (Constants.kLoggingDrivetrain) {
+            drivetrainPoseLogger.append(new double[]{robotState.fieldToVehicle.getX(), robotState.fieldToVehicle.getY(), robotState.fieldToVehicle.getRotation().getDegrees()});
+            drivetrainChassisSpeedsLogger.append(new double[]{robotState.deltaVehicle.vxMetersPerSecond, robotState.deltaVehicle.vyMetersPerSecond, robotState.deltaVehicle.omegaRadiansPerSecond});
+            gyroPitchLogger.append(pigeon.getPitchValue());
+            gyroRollLogger.append(pigeon.getRollValue());
+        }
     }
 
     @Override
     public void resetOdometry(Pose2d pose) {
-
+        train.seedFieldRelative(pose);
+        updateRobotState();
     }
 
     @Override
     public void zeroSensors(Pose2d pose) {
-
+        resetOdometry(pose);
+        startingPose = pose;
+        chassisSpeed = new ChassisSpeeds();
+        isBraking = false;
     }
 
     @Override
     public void stop() {
-
+        // TODO: Figure out what would be considered stop with the swervedrivetrain.
     }
 
     @Override
     public boolean testSubsystem() {
+        // TODO: Figure out what would be considered testing system.
         return true;
     }
 
@@ -95,6 +150,8 @@ public class CTRESwerveDrive extends Drive {
         speeds_request.Speeds = new ChassisSpeeds(forward, strafe, rotation);
 
         request = speeds_request;
+
+        setOpenLoop(null);
     }
 
     @Override
@@ -102,7 +159,8 @@ public class CTRESwerveDrive extends Drive {
         isBraking = braking;
 
         if (braking) {
-            request = new SwerveRequest.SwerveDriveBrake();
+            isBraking = true;
+            setOpenLoop(DriveSignal.BRAKE);
         }
     }
 
