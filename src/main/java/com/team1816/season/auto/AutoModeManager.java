939bbed209b6726277d5f1bfecdc37b85530112a
tagciccone <tagciccone@gmail.com>
2024-01-25 10:52:40 -0600

    janky but working implementation

--- src/main/java/com/team1816/season/auto/AutoModeManager.java
+++ src/main/java/com/team1816/season/auto/AutoModeManager.java
@@ -3,13 +3,21 @@ package com.team1816.season.auto;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import com.team1816.lib.auto.Color;
-import com.team1816.lib.auto.PathFinder;
 import com.team1816.lib.auto.modes.AutoMode;
 import com.team1816.lib.auto.modes.DoNothingMode;
 import com.team1816.lib.auto.modes.DriveStraightMode;
+import com.team1816.lib.auto.paths.AutoPath;
 import com.team1816.lib.util.logUtil.GreenLogger;
 import com.team1816.season.auto.modes.*;
-import com.team1816.season.configuration.Constants;
+import com.team1816.season.auto.paths.toNoteOne.BottomSpeakerToNoteOnePath;
+import com.team1816.season.auto.paths.toNoteOne.MiddleSpeakerToNoteOnePath;
+import com.team1816.season.auto.paths.toNoteOne.TopSpeakerToNoteOnePath;
+import com.team1816.season.auto.paths.toNoteThree.BottomSpeakerToNoteThreePath;
+import com.team1816.season.auto.paths.toNoteThree.MiddleSpeakerToNoteThreePath;
+import com.team1816.season.auto.paths.toNoteThree.TopSpeakerToNoteThreePath;
+import com.team1816.season.auto.paths.toNoteTwo.BottomSpeakerToNoteTwoPath;
+import com.team1816.season.auto.paths.toNoteTwo.MiddleSpeakerToNoteTwoPath;
+import com.team1816.season.auto.paths.toNoteTwo.TopSpeakerToNoteTwoPath;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
@@ -33,6 +41,17 @@ public class AutoModeManager {
     private Color teamColor;
 
     /**
+     * Properties: Dynamic Auto
+     */
+    private final SendableChooser<ShootPos> startPosChooser;
+    private final SendableChooser<DesiredCollect> firstCollectChooser;
+    private final SendableChooser<ShootPos> firstShootChooser;
+    private ShootPos desiredStart;
+    private DesiredCollect desiredFirstCollect;
+    private ShootPos desiredFirstShoot;
+
+
+    /**
      * Properties: Execution
      */
     private AutoMode autoMode;
@@ -48,6 +67,9 @@ public class AutoModeManager {
         robotState = rs;
         autoModeChooser = new SendableChooser<>(); // Shuffleboard dropdown menu to choose desired auto mode
         sideChooser = new SendableChooser<>(); // Shuffleboard dropdown menu to choose desired side / bumper color
+        startPosChooser = new SendableChooser<>(); // Shuffleboard dropdown menu to choose desired starting position for dynamic auto
+        firstCollectChooser = new SendableChooser<>(); // Shuffleboard dropdown menu to choose first desired note collected for dynamic auto
+        firstShootChooser = new SendableChooser<>(); // Shuffleboard dropdown menu to choose first desired shoot position for dynamic auto
 
         SmartDashboard.putData("Auto mode", autoModeChooser); // appends chooser to shuffleboard
 
@@ -63,6 +85,27 @@ public class AutoModeManager {
 
         sideChooser.setDefaultOption(Color.BLUE.name(), Color.BLUE); // initialize options
         sideChooser.addOption(Color.RED.name(), Color.RED); // initialize options
+
+        /**
+         * Dynamic Auto
+         */
+        SmartDashboard.putData("Dynamic Start", startPosChooser);
+        // Doing manually because can't start @ amp
+        startPosChooser.setDefaultOption(ShootPos.TOP_SPEAKER.name(), ShootPos.TOP_SPEAKER);
+        startPosChooser.addOption(ShootPos.MIDDLE_SPEAKER.name(), ShootPos.MIDDLE_SPEAKER);
+        startPosChooser.addOption(ShootPos.BOTTOM_SPEAKER.name(), ShootPos.BOTTOM_SPEAKER);
+
+        SmartDashboard.putData("First collected note", firstCollectChooser);
+        for (DesiredCollect noteToCollect : DesiredCollect.values()) {
+            firstCollectChooser.addOption(noteToCollect.name(), noteToCollect);
+        }
+        firstCollectChooser.setDefaultOption(DesiredCollect.TOP_NOTE.name(), DesiredCollect.TOP_NOTE);
+
+        SmartDashboard.putData("First shooting position", firstShootChooser);
+        for (ShootPos shootPos : ShootPos.values()) {
+            firstShootChooser.addOption(shootPos.name(), shootPos);
+        }
+        firstShootChooser.setDefaultOption(ShootPos.TOP_SPEAKER.name(), ShootPos.TOP_SPEAKER);
 
         reset();
     }
@@ -85,17 +128,26 @@ public class AutoModeManager {
      */
     public boolean update() {
         DesiredAuto selectedAuto = autoModeChooser.getSelected();
+        ShootPos selectedStartPos = startPosChooser.getSelected();
+        DesiredCollect selectedFirstCollect = firstCollectChooser.getSelected();
+        ShootPos selectedFirstShoot = firstShootChooser.getSelected();
+
         Color selectedColor = Color.BLUE;
+
+
         if (RobotBase.isSimulation()) {
             selectedColor = sideChooser.getSelected();
         } else if (RobotBase.isReal()) {
             var dsAlliance = DriverStation.getAlliance().isPresent() ? DriverStation.getAlliance().get() : sideChooser.getSelected(); //ternary hell
             selectedColor = (dsAlliance == DriverStation.Alliance.Red) ? Color.RED : Color.BLUE;
         }
+
         boolean autoChanged = desiredAuto != selectedAuto;
         boolean colorChanged = teamColor != selectedColor;
+        boolean dynamicAutoChanged = selectedStartPos == desiredStart && selectedFirstCollect == desiredFirstCollect && selectedFirstShoot == desiredFirstShoot;
+
         // if auto has been changed, update selected auto mode + thread
-        if (autoChanged || colorChanged) {
+        if (autoChanged || colorChanged || dynamicAutoChanged) {
             if (autoChanged) {
                 GreenLogger.log(
                     "Auto changed from: " + desiredAuto + ", to: " + selectedAuto.name()
@@ -104,7 +156,13 @@ public class AutoModeManager {
             if (colorChanged) {
                 GreenLogger.log("Robot color changed from: " + teamColor + ", to: " + selectedColor);
             }
-            autoMode = generateAutoMode(selectedAuto, selectedColor);
+
+            if (selectedAuto == DesiredAuto.TWO_SCORE) {
+                autoMode = generateDynamicAutoMode(selectedAuto, selectedColor, selectedStartPos, selectedFirstCollect, selectedFirstShoot);
+            } else {
+                autoMode = generateAutoMode(selectedAuto, selectedColor);
+            }
+
             autoModeThread = new Thread(autoMode::run);
         }
         desiredAuto = selectedAuto;
@@ -181,9 +239,22 @@ public class AutoModeManager {
         DRIVE_STRAIGHT,
 
         // New Auto Modes : 20??
-        TEST
+        TEST,
+        TWO_SCORE
     }
 
+    enum ShootPos {
+        TOP_SPEAKER,
+        MIDDLE_SPEAKER,
+        BOTTOM_SPEAKER,
+        AMP
+    }
+
+    enum DesiredCollect {
+        TOP_NOTE,
+        MIDDLE_NOTE,
+        BOTTOM_NOTE
+    }
     /**
      * Generates each AutoMode by demand
      *
@@ -205,5 +276,59 @@ public class AutoModeManager {
                 GreenLogger.log("Defaulting to drive straight mode");
                 return new DriveStraightMode();
         }
+    }
+
+    private AutoMode generateDynamicAutoMode(DesiredAuto mode, Color color, ShootPos selectedStart, DesiredCollect selectedCollect, ShootPos selectedShoot) {
+        return new TwoScoreMode(generateDynamicPathList(selectedStart,selectedCollect,selectedShoot));
+    }
+
+    private List<AutoPath> generateDynamicPathList(ShootPos start, DesiredCollect collectOne, ShootPos shootOne) {
+        //TODO generalize?
+
+        // Start -> first collect
+        AutoPath startToCollect;
+        if (start == ShootPos.TOP_SPEAKER) {
+            startToCollect = switch (collectOne) {
+                case TOP_NOTE -> new TopSpeakerToNoteOnePath();
+                case MIDDLE_NOTE -> new TopSpeakerToNoteTwoPath();
+                case BOTTOM_NOTE -> new TopSpeakerToNoteThreePath();
+            };
+        } else if (start == ShootPos.MIDDLE_SPEAKER) {
+            startToCollect = switch (collectOne) {
+                case TOP_NOTE -> new MiddleSpeakerToNoteOnePath();
+                case MIDDLE_NOTE -> new MiddleSpeakerToNoteTwoPath();
+                case BOTTOM_NOTE -> new MiddleSpeakerToNoteThreePath();
+            };
+        } else {
+            startToCollect = switch (collectOne) {
+                case TOP_NOTE -> new BottomSpeakerToNoteOnePath();
+                case MIDDLE_NOTE -> new BottomSpeakerToNoteTwoPath();
+                case BOTTOM_NOTE -> new BottomSpeakerToNoteThreePath();
+            };
+        }
+
+        // Collect one -> Shoot one
+        AutoPath collectToShoot; //TODO make amp paths
+        if (collectOne == DesiredCollect.TOP_NOTE) {
+            collectToShoot = switch (shootOne) {
+                case TOP_SPEAKER -> new TopSpeakerToNoteOnePath().withInversedWaypoints();
+                case MIDDLE_SPEAKER -> new MiddleSpeakerToNoteOnePath().withInversedWaypoints();
+                default -> new BottomSpeakerToNoteOnePath().withInversedWaypoints();
+            };
+        } else if (collectOne == DesiredCollect.MIDDLE_NOTE) {
+            collectToShoot = switch (shootOne) {
+                case TOP_SPEAKER -> new TopSpeakerToNoteTwoPath().withInversedWaypoints();
+                case MIDDLE_SPEAKER -> new MiddleSpeakerToNoteTwoPath().withInversedWaypoints();
+                default -> new BottomSpeakerToNoteTwoPath().withInversedWaypoints();
+            };
+        } else {
+            collectToShoot = switch (shootOne) {
+                case TOP_SPEAKER -> new TopSpeakerToNoteThreePath().withInversedWaypoints();
+                case MIDDLE_SPEAKER -> new MiddleSpeakerToNoteThreePath().withInversedWaypoints();
+                default -> new BottomSpeakerToNoteThreePath().withInversedWaypoints();
+            };
+        }
+
+        return List.of(startToCollect, collectToShoot);
     }
 }
--- src/main/java/com/team1816/season/auto/modes/TwoScoreMode.java
+++ src/main/java/com/team1816/season/auto/modes/TwoScoreMode.java
@@ -5,20 +5,31 @@ import com.team1816.lib.auto.actions.SeriesAction;
 import com.team1816.lib.auto.actions.TrajectoryAction;
 import com.team1816.lib.auto.actions.WaitAction;
 import com.team1816.lib.auto.modes.AutoMode;
+import com.team1816.lib.auto.paths.AutoPath;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class TwoScoreMode extends AutoMode {
-    public TwoScoreMode(List<TrajectoryAction> paths) {
-        super(paths);
+    public TwoScoreMode(List<AutoPath> paths) {
+        super(encapsulateAutoPaths(paths));
     }
 
     @Override
     protected void routine() throws AutoModeEndedException {
         runAction(
             new SeriesAction(
-
+                trajectoryActions.get(0), trajectoryActions.get(1)
             )
         );
+    }
+
+    private static List<TrajectoryAction> encapsulateAutoPaths(List<AutoPath> paths) {
+        List<TrajectoryAction> trajectories = new ArrayList<TrajectoryAction>();
+
+        paths.forEach(path -> {
+            trajectories.add(new TrajectoryAction(path));
+        });
+        return trajectories;
     }
 }
