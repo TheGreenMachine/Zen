e092ec656413a05b40aedca71f35f0a99d9593b3
Ethan Liao <ethancliao@gmail.com>
2024-01-25 17:23:27 -0600

    finished shooter class except for pivot

--- src/main/java/com/team1816/season/subsystems/Shooter.java
+++ src/main/java/com/team1816/season/subsystems/Shooter.java
@@ -6,6 +6,8 @@ import com.team1816.lib.hardware.components.motor.configurations.GreenControlMod
 import com.team1816.lib.subsystems.Subsystem;
 import com.team1816.season.configuration.Constants;
 import com.team1816.season.states.RobotState;
+import edu.wpi.first.util.datalog.DoubleLogEntry;
+import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.DigitalInput;
 
 public class Shooter extends Subsystem {
@@ -34,8 +36,29 @@ public class Shooter extends Subsystem {
     private FEEDER_STATE desiredFeederState = FEEDER_STATE.STOP;
     private boolean rollerOutputsChanged = false;
     private boolean feederOutputsChanged = false;
-    private double rollerVelocity = 0;
-    private double feederVelocity = 0;
+    private double actualRollerVelocity = 0;
+    private double actualFeederVelocity = 0;
+    private double rollerCurrentDraw;
+    private double feederCurrentDraw;
+
+    /**
+     * Constants
+     */
+    private static final double velocityErrorMargin = factory.getConstant(NAME, "velocityErrorMargin", 0.1);
+    private static final double rollerSpeakerShootSpeed = factory.getConstant(NAME, "rollerSpeakerShootSpeed", 0.70);
+    private static final double rollerAmpShootSpeed = factory.getConstant(NAME, "rollerAmpShootSpeed", 0.40);
+    private final double feederSpeakerShootSpeed = factory.getConstant(NAME, "feederSpeakerShootSpeed", 0.70);
+    private final double feederAmpShootSpeed = factory.getConstant(NAME, "feederAmpShootSpeed", 0.40);
+
+    /**
+     * Logging
+     */
+    private DoubleLogEntry actualRollerVelocityLogger;
+    private DoubleLogEntry actualFeederVelocityLogger;
+    private DoubleLogEntry rollerCurrentDrawLogger;
+    private DoubleLogEntry feederCurrentDrawLogger;
+    private DoubleLogEntry desiredRollerVelocityLogger;
+    private DoubleLogEntry desiredFeederVelocityLogger;
 
     /**
      * Base constructor needed to instantiate a shooter
@@ -50,6 +73,15 @@ public class Shooter extends Subsystem {
         noteSensor = new DigitalInput((int) factory.getConstant(NAME, "noteSensorChannel", 0));
 
         // shootPower = factory.getConstant(NAME, "shootPower", 0.70);
+
+        if (Constants.kLoggingRobot) {
+            actualRollerVelocityLogger = new DoubleLogEntry(DataLogManager.getLog(), "Shooter/Roller/actualRollerVelocity");
+            actualFeederVelocityLogger = new DoubleLogEntry(DataLogManager.getLog(), "Shooter/Feeder/actualFeederVelocity");
+            rollerCurrentDrawLogger = new DoubleLogEntry(DataLogManager.getLog(), "Shooter/Roller/rollerMotorCurrentDraw");
+            feederCurrentDrawLogger = new DoubleLogEntry(DataLogManager.getLog(), "Shooter/Feeder/feederMotorCurrentDraw");
+            desiredRollerVelocityLogger = new DoubleLogEntry(DataLogManager.getLog(), "Shooter/Roller/desiredRollerVelocity");
+            desiredFeederVelocityLogger = new DoubleLogEntry(DataLogManager.getLog(), "Shooter/Roller/desiredFeederVelocity");
+        }
     }
 
     /**
@@ -73,14 +105,30 @@ public class Shooter extends Subsystem {
     }
 
     /**
+     * Sets the desired state of the roller and feeder
+     *
+     * @param desiredRollerState ROLLER_STATE
+     * @param desiredFeederState FEEDER_STATE
+     */
+    public void setDesiredState(ROLLER_STATE desiredRollerState, FEEDER_STATE desiredFeederState) {
+        this.desiredRollerState = desiredRollerState;
+        this.desiredFeederState = desiredFeederState;
+
+        rollerOutputsChanged = true;
+        feederOutputsChanged = true;
+    }
+
+    /**
      * Reads actual outputs from shooter motors
      *
      * @see Subsystem#readFromHardware()
      */
     @Override
     public void readFromHardware() {
-        rollerVelocity = rollerMotor.getSensorVelocity(0);
-        feederVelocity = feederMotor.getSensorVelocity(0);
+        actualRollerVelocity = rollerMotor.getSensorVelocity(0);
+        actualFeederVelocity = feederMotor.getSensorVelocity(0);
+        rollerCurrentDraw = rollerMotor.getMotorOutputCurrent();
+        feederCurrentDraw = feederMotor.getMotorOutputCurrent();
 
         if (robotState.actualRollerState != desiredRollerState) {
             robotState.actualRollerState = desiredRollerState;
@@ -90,8 +138,15 @@ public class Shooter extends Subsystem {
             robotState.actualFeederState = desiredFeederState;
         }
 
-        if (robotState.isBeamBreakTriggered == noteSensor.get()) {
-            robotState.isBeamBreakTriggered = !noteSensor.get();
+        if (robotState.isBeamBreakTriggered != isBeamBreakTriggered()) {
+            robotState.isBeamBreakTriggered = isBeamBreakTriggered();
+        }
+
+        if (Constants.kLoggingRobot) {
+            actualRollerVelocityLogger.append(actualRollerVelocity);
+            actualFeederVelocityLogger.append(actualFeederVelocity);
+            rollerCurrentDrawLogger.append(rollerCurrentDraw);
+            feederCurrentDrawLogger.append(feederCurrentDraw);
         }
     }
 
@@ -104,20 +159,37 @@ public class Shooter extends Subsystem {
     public void writeToHardware() {
         if (rollerOutputsChanged) {
             rollerOutputsChanged = false;
-            switch (desiredRollerState) { // Everything in here is totally wrong right now
+            double desiredRollerVelocity = 0;
+            switch (desiredRollerState) {
                 case STOP -> {
-                    rollerMotor.set(GreenControlMode.PERCENT_OUTPUT, 0);
+                    desiredRollerVelocity = 0;
                 }
                 case SHOOT_SPEAKER -> {
-                    rollerMotor.set(GreenControlMode.PERCENT_OUTPUT, 0.70);
+                    desiredRollerVelocity = rollerSpeakerShootSpeed;
+                }
+                case SHOOT_AMP -> {
+                    desiredRollerVelocity = rollerAmpShootSpeed;
                 }
             }
+            rollerMotor.set(GreenControlMode.PERCENT_OUTPUT, desiredRollerVelocity);
+            desiredRollerVelocityLogger.append(desiredRollerVelocity);
         }
         if (feederOutputsChanged) {
             feederOutputsChanged = false;
+            double desiredFeederVelocity = 0;
             switch (desiredFeederState) {
-
+                case STOP -> {
+                    desiredFeederVelocity = 0;
+                }
+                case SHOOT_SPEAKER -> {
+                    desiredFeederVelocity = feederSpeakerShootSpeed;
+                }
+                case SHOOT_AMP -> {
+                    desiredFeederVelocity = feederAmpShootSpeed;
+                }
             }
+            feederMotor.set(GreenControlMode.PERCENT_OUTPUT, desiredFeederVelocity);
+            desiredFeederVelocityLogger.append(desiredFeederVelocity);
         }
     }
 
@@ -134,6 +206,15 @@ public class Shooter extends Subsystem {
     @Override
     public boolean testSubsystem() {
         return false;
+    }
+
+    /**
+     * Returns whether the beam break is triggered
+     *
+     * @return whether the beam break is triggered
+     */
+    public boolean isBeamBreakTriggered() {
+        return !noteSensor.get();
     }
 
     /**
@@ -159,14 +240,14 @@ public class Shooter extends Subsystem {
      */
     public enum ROLLER_STATE {
         STOP(0),
-        SHOOT_SPEAKER(factory.getConstant(NAME, "rollerSpeakerShootSpeed", 0.70)),
-        SHOOT_AMP(factory.getConstant(NAME, "rollerAmpShootSpeed", 0.40));
+        SHOOT_SPEAKER(rollerSpeakerShootSpeed),
+        SHOOT_AMP(rollerAmpShootSpeed);
         final double velocity;
         ROLLER_STATE (double velocity) {
             this.velocity = velocity;
         }
         public boolean inDesiredSpeedRange (double actualVelocity) {
-            return actualVelocity < 1.1 * velocity && actualVelocity > 0.9 * velocity;
+            return actualVelocity < (1+velocityErrorMargin) * velocity && actualVelocity > (1-velocityErrorMargin) * velocity;
         }
     }
 
@@ -176,8 +257,6 @@ public class Shooter extends Subsystem {
     public enum FEEDER_STATE {
         STOP,
         SHOOT_SPEAKER,
-        SHOOT_AMP,
-        REV_SPEAKER,
-        REV_AMP
+        SHOOT_AMP
     }
 }
