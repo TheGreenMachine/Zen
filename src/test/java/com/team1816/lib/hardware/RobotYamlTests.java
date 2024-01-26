de9ff9c96586d01be33f302e6ce0ad0ff3799cca
tagciccone <tagciccone@gmail.com>
2024-01-26 14:25:35 -0600

    update RobotYamlTests

--- src/test/java/com/team1816/lib/hardware/RobotYamlTests.java
+++ src/test/java/com/team1816/lib/hardware/RobotYamlTests.java
@@ -1,44 +1,37 @@
-//package com.team1816.lib.hardware;
-//
-//import static org.junit.Assert.assertNotNull;
-//
-//import org.junit.Test;
-//
-//public class RobotYamlTests {
-//
-//    @Test
-//    public void defaultYamlTest() {
-//        loadConfig("default");
-//    }
-//
-//    @Test
-//    public void cheezeCurdYamlTest() {
-//        loadConfig("CheezeCurd");
-//    }
-//
-//    @Test
-//    public void zodiacYamlTest() {
-//        loadConfig("zodiac_swerve");
-//    }
-//
-//    @Test
-//    public void zodiacProYamlTest() {
-//        loadConfig("zodiac_pro");
-//    }
-//
-//    private void loadConfig(String configName) {
-//        YamlConfig config = null;
-//        try {
-//            config =
-//                YamlConfig.loadFrom(
-//                    this.getClass()
-//                        .getClassLoader()
-//                        .getResourceAsStream(configName + ".config.yml")
-//                );
-//        } catch (ConfigIsAbstractException e) {
-//            e.printStackTrace();
-//        }
-//        assertNotNull(config);
-//        System.out.println(config);
-//    }
-//}
+package com.team1816.lib.hardware;
+
+
+import com.team1816.lib.hardware.factory.YamlConfig;
+import org.junit.jupiter.api.Test;
+import org.yaml.snakeyaml.error.YAMLException;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class RobotYamlTests {
+
+    @Test
+    public void defaultYamlTest() {
+        loadConfig("default");
+    }
+
+    @Test
+    public void zodiacProYamlTest() {
+        loadConfig("zodiac_pro");
+    }
+
+    private void loadConfig(String configName) {
+        RobotConfiguration config = null;
+        try {
+            config =
+                YamlConfig.loadFrom(
+                    this.getClass()
+                        .getClassLoader()
+                        .getResourceAsStream("yaml/" +configName + ".config.yml")
+                );
+        } catch (YAMLException e) {
+            e.printStackTrace();
+        }
+        assertNotNull(config);
+        System.out.println(config);
+    }
+}
