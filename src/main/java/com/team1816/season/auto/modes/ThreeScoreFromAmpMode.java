54c432da75e5831bcf1dd58654a6ce25825b8fc9
tagciccone <tagciccone@gmail.com>
2024-02-13 17:05:22 -0600

    Three score from amp

--- src/main/java/com/team1816/season/auto/modes/ThreeScoreFromAmpMode.java
+++ src/main/java/com/team1816/season/auto/modes/ThreeScoreFromAmpMode.java
@@ -4,25 +4,25 @@ import com.team1816.lib.auto.AutoModeEndedException;
 import com.team1816.lib.auto.DynamicAutoUtil;
 import com.team1816.lib.auto.actions.ParallelAction;
 import com.team1816.lib.auto.actions.SeriesAction;
-import com.team1816.lib.auto.actions.TrajectoryAction;
 import com.team1816.lib.auto.modes.AutoMode;
 import com.team1816.lib.auto.paths.DynamicAutoPath;
-import com.team1816.season.auto.actions.*;
-import com.team1816.season.auto.paths.StartToAmpPath;
+import com.team1816.season.auto.actions.CollectAction;
+import com.team1816.season.auto.actions.ShootAction;
+import com.team1816.season.auto.actions.ShootAmpAction;
+import com.team1816.season.auto.actions.ShootSpeakerAction;
 import com.team1816.season.subsystems.Collector;
 import com.team1816.season.subsystems.Shooter;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class TwoScoreFromAmpMode extends AutoMode {
+public class ThreeScoreFromAmpMode extends AutoMode {
     ArrayList<DynamicAutoPath> paths;
-
-    public TwoScoreFromAmpMode(List<DynamicAutoPath> paths) {
-        super(DynamicAutoUtil.encapsulateAutoPaths(paths)); //TODO: Amp paths need reversal safety now.
+    public ThreeScoreFromAmpMode(List<DynamicAutoPath> paths) {
+        super(DynamicAutoUtil.encapsulateAutoPaths(paths));
         this.paths = new ArrayList<>(paths);
         if (trajectoryActions.size() > 3) {
-            trajectoryActions.subList(3, trajectoryActions.size()).clear();
+            trajectoryActions.subList(4, trajectoryActions.size()).clear();
         }
     }
 
@@ -38,8 +38,12 @@ public class TwoScoreFromAmpMode extends AutoMode {
                         ),
                         trajectoryActions.get(2),
                         paths.get(2).isAmpPath() ? new ShootAmpAction() : new ShootSpeakerAction(),
-                        new ShootAction(Shooter.ROLLER_STATE.STOP, Shooter.FEEDER_STATE.STOP, Shooter.PIVOT_STATE.STOW)
-                )
-        );
+                        new ParallelAction(
+                                new CollectAction(Collector.COLLECTOR_STATE.INTAKE),
+                                trajectoryActions.get(3)
+                        ),
+                        trajectoryActions.get(4),
+                        paths.get(4).isAmpPath() ? new ShootAmpAction() : new ShootSpeakerAction(),
+                        new ShootAction(Shooter.ROLLER_STATE.STOP, Shooter.FEEDER_STATE.STOP, Shooter.PIVOT_STATE.STOW)));
     }
 }
