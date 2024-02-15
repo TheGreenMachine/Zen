e3cd374a9afdbce2f9573da3a2752e12b4658cfc
cookienthusiast <louckselena@gmail.com>
2024-02-15 17:33:35 -0600

    bottom middle ejects mode, action, path

---
+++ src/main/java/com/team1816/season/auto/paths/nonDynamic/BottomSpeakerToFive.java
@@ -0,0 +1,34 @@
+package com.team1816.season.auto.paths.nonDynamic;
+
+import com.team1816.lib.auto.paths.AutoPath;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+
+import java.util.List;
+
+public class BottomSpeakerToFive extends AutoPath {
+    @Override
+    protected List<Pose2d> getWaypoints() {
+        return List.of(
+            new Pose2d(0.75, 4.31, Rotation2d.fromDegrees(-60)),
+            new Pose2d(2.33, 2.56, Rotation2d.fromDegrees(-35)),
+            new Pose2d(4.79, 1.47, Rotation2d.fromDegrees(-20)),
+            new Pose2d(7.77, 0.84, Rotation2d.fromDegrees(-10))
+        );
+    }
+
+    @Override
+    protected List<Rotation2d> getWaypointHeadings() {
+        return List.of(
+            Rotation2d.fromDegrees(0),
+            Rotation2d.fromDegrees(0),
+            Rotation2d.fromDegrees(0),
+            Rotation2d.fromDegrees(0)
+        );
+    }
+
+    @Override
+    protected boolean usingApp() {
+        return true;
+    }
+}
