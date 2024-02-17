c99a047f1a1eaa98056bff6634739b3ca154f8ba
tagciccone <tagciccone@gmail.com>
2024-02-17 13:52:44 -0600

    post-match fixes

--- src/main/java/com/team1816/season/auto/modes/TestMode.java
+++ src/main/java/com/team1816/season/auto/modes/TestMode.java
@@ -4,17 +4,6 @@ import com.team1816.lib.auto.AutoModeEndedException;
 import com.team1816.lib.auto.actions.SeriesAction;
 import com.team1816.lib.auto.actions.TrajectoryAction;
 import com.team1816.lib.auto.modes.AutoMode;
-import com.team1816.season.auto.paths.StartToAmpPath;
-import com.team1816.season.auto.paths.toNoteOne.AmpToNoteOnePath;
-import com.team1816.season.auto.paths.toNoteOne.BottomSpeakerToNoteOnePath;
-import com.team1816.season.auto.paths.toNoteOne.MiddleSpeakerToNoteOnePath;
-import com.team1816.season.auto.paths.toNoteOne.TopSpeakerToNoteOnePath;
-import com.team1816.season.auto.paths.toNoteThree.AmpToNoteThreePath;
-import com.team1816.season.auto.paths.toNoteThree.BottomSpeakerToNoteThreePath;
-import com.team1816.season.auto.paths.toNoteThree.MiddleSpeakerToNoteThreePath;
-import com.team1816.season.auto.paths.toNoteThree.TopSpeakerToNoteThreePath;
-import com.team1816.season.auto.paths.toNoteTwo.AmpToNoteTwoPath;
-import com.team1816.season.auto.paths.toNoteTwo.AmpToNoteTwoTopPath;
 import com.team1816.season.auto.paths.toNoteTwo.MiddleSpeakerToNoteTwoPath;
 import com.team1816.season.auto.paths.toNoteTwo.TopSpeakerToNoteTwoTopPath;
 
