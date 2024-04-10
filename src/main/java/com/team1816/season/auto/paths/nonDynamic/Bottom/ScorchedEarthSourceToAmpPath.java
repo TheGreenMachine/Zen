84d8f5d8b3195bc4be7f12d256b9e24b3663351d
tagciccone <tagciccone@gmail.com>
2024-04-10 17:41:06 -0500

    SCEA 2 done

--- src/main/java/com/team1816/season/auto/paths/nonDynamic/Bottom/ScorchedEarthSourceToAmpPath.java
+++ src/main/java/com/team1816/season/auto/paths/nonDynamic/Bottom/ScorchedEarthSourceToAmpPath.java
@@ -15,20 +15,22 @@ public class ScorchedEarthSourceToAmpPath extends AutoPath {
     @Override
     protected List<Pose2d> getWaypoints() {
         return List.of(
-                new Pose2d(0.54, 2.05, Rotation2d.fromDegrees(-12)),
-                new Pose2d(7.02, 1.25, Rotation2d.fromDegrees(-12)),
-                new Pose2d(8.65, 1, Rotation2d.fromDegrees(90)),
-                new Pose2d(8.65, 7.5, Rotation2d.fromDegrees(90))
+                new Pose2d(0.48, 2.06, Rotation2d.fromDegrees(0)),
+                new Pose2d(6.94, 1.03, Rotation2d.fromDegrees(-5)),
+                new Pose2d(8.29, 1.15, Rotation2d.fromDegrees(90)),
+                new Pose2d(8.29, 7.46, Rotation2d.fromDegrees(90)),
+                new Pose2d(7, 7.46, Rotation2d.fromDegrees(180))
         );
     }
 
     @Override
     protected List<Rotation2d> getWaypointHeadings() {
         return List.of(
-                Rotation2d.fromDegrees(-90),
-                Rotation2d.fromDegrees(-35-90),
-                Rotation2d.fromDegrees(-35-90),
-                Rotation2d.fromDegrees(-35-90)
+                Rotation2d.fromDegrees(0),
+                Rotation2d.fromDegrees(35),
+                Rotation2d.fromDegrees(35),
+                Rotation2d.fromDegrees(35),
+                Rotation2d.fromDegrees(35)
         );
     }
 
