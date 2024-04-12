8ee2f0384958f10e83ddebd66078fbc1d895398b
cookienthusiast <louckselena@gmail.com>
2024-04-12 18:08:41 -0500

    eject speeds

--- src/main/java/com/team1816/season/auto/actions/EjectAction.java
+++ src/main/java/com/team1816/season/auto/actions/EjectAction.java
@@ -9,7 +9,7 @@ public class EjectAction extends SeriesAction {
     public EjectAction() {
         super(
             new SeriesAction(
-                new ShootAction(Shooter.ROLLER_STATE.SHOOT_SPEAKER, Shooter.FEEDER_STATE.SHOOT, Shooter.PIVOT_STATE.STOW),
+                new ShootAction(Shooter.ROLLER_STATE.EJECT, Shooter.FEEDER_STATE.SHOOT, Shooter.PIVOT_STATE.STOW),
                 new WaitAction(0.1),
                 new ShootAction(Shooter.ROLLER_STATE.SHOOT_SPEAKER, Shooter.FEEDER_STATE.TRANSFER, Shooter.PIVOT_STATE.STOW)
                     //TODO @Elena we had made it IDLE when we thought it was a problem with that, was the beambreak instead - if you find that this causes issues just go ahead and switch it back to IDLE
